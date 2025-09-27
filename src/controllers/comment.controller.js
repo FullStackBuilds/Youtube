d4b0ed0da50690c1e38643dc94bc047cc9f30bb0
Sakshamgupta12202014 <gupta.saksham0401@gmail.com>
2025-09-27 22:56:20 +0530

    Implement comment management functionality

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,3 +1,7 @@
+import { Comment } from "../models/comment.model";
+import ApiError from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
 export const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const { videoId } = req.params;
@@ -6,12 +10,89 @@ export const getVideoComments = asyncHandler(async (req, res) => {
 
 export const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+
+    const { videoId } = req.params;
+    if (!videoId) {
+        throw new ApiError(404, "Video id is required to add comment");
+    }
+
+    const { content } = req?.body;
+    if (!content || content.trim() === "") {
+        throw new ApiError(400, "comment cannot be empty for a video");
+    }
+
+    const newComment = await Comment.create({
+        content,
+        video: videoId,
+        owner: req?.user?._id,
+    });
+
+    if (!newComment) {
+        throw new ApiError(500, "Failed to add comment under video");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                newComment,
+                "Successfully added comment under video"
+            )
+        );
 });
 
 export const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const { commentId } = req.params;
+    if (!commentId) {
+        throw new ApiError(404, "Comment id is required to update comment");
+    }
+
+    const { newContent } = req.body;
+    if (!newContent || newContent.trim() === "") {
+        throw new ApiError(400, "comment cannot be empty");
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(
+        commentId,
+        {
+            $set: {
+                content: newContent,
+            },
+        },
+        { new: true }
+    );
+
+    if (!updatedComment) {
+        throw new ApiError(500, "Failed to update comment");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, updatedComment, "Successfully updated comment")
+        );
 });
 
 export const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const { commentId } = req.params;
+    if (!commentId) {
+        throw new ApiError(404, "Comment id is required to delete comment");
+    }
+
+    const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+    if (!deletedComment) {
+        return res
+            .status(404)
+            .json(new ApiResponse(404, {}, "Comment not found"));
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, deletedComment, "Successfully updated comment")
+        );
 });
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,20 +1,26 @@
 export const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
+    const { videoId } = req.params;
     //TODO: toggle like on video
-})
+});
 
 export const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
+    const { commentId } = req.params;
     //TODO: toggle like on comment
-
-})
+});
 
 export const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
+    const { tweetId } = req.params;
     //TODO: toggle like on tweet
-}
-)
+});
 
 export const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
-})
+
+    const user = req?.user;
+
+    if (!user || !user._id) {
+        throw new ApiError(401, "User not authenticated");
+    }
+
+    
+});
